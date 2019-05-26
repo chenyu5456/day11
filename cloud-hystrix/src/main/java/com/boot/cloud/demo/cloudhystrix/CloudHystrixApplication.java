61832d2f76396ab78d4cdcca435a103e970ca83b
chenyu5456 <545641154@qq.com>
2019-05-26 18:18:01 +0800

    更新

--- cloud-hystrix/src/main/java/com/boot/cloud/demo/cloudhystrix/CloudHystrixApplication.java
+++ cloud-hystrix/src/main/java/com/boot/cloud/demo/cloudhystrix/CloudHystrixApplication.java
@@ -2,8 +2,12 @@ package com.boot.cloud.demo.cloudhystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CloudHystrixApplication {
 
     public static void main(String[] args) {
--- cloud-hystrix/src/main/java/com/boot/cloud/demo/cloudhystrix/controller/HystrixController.java
+++ cloud-hystrix/src/main/java/com/boot/cloud/demo/cloudhystrix/controller/HystrixController.java
@@ -1,9 +1,13 @@
 package com.boot.cloud.demo.cloudhystrix.controller;
 
 import com.boot.cloud.demo.cloudhystrix.vo.UserVo;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,15 +17,51 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+//    @HystrixCommand(fallbackMethod = "fallback", commandProperties = {@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "2000")})
+    @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("hi/{id}")
-    public UserVo getUser(@PathVariable("id") Integer userId){
-        UserVo userVo = restTemplate.getForObject("http//provider/user/" + userId, UserVo.class);
+    public UserVo getUser(@PathVariable("id") Integer userId, @RequestParam int number){
+        if(number % 2 == 0){
+            UserVo vo = new UserVo();
+            vo.setUserName("success");
+            return vo;
+        }
+        UserVo userVo = restTemplate.getForObject("http://provider/user/" + userId, UserVo.class);
+        return userVo;
+    }
+    @HystrixCommand
+    @GetMapping("hi1/{id}")
+    public String getUser1(){
+        return "hi1";
+    }
+    @HystrixCommand
+    @GetMapping("hi2/{id}")
+    public String getUser2(){
+        return "hi2";
+    }
+
+    /**
+     * 降级处理的方法
+     * @param id
+     * @return
+     */
+    private UserVo fallback(Integer id){
+        UserVo userVo = new UserVo();
+        userVo.setUserName("系统繁忙，请稍后再试");
         return userVo;
     }
 
+    /**
+     * 默认降级方法
+     * @return
+     */
+    private String defaultFallback(){
+        return "全局->系统繁忙，请稍后再试";
+    }
 }
