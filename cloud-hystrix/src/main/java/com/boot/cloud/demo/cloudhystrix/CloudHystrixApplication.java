4d7ed5b94333b784994b2fdc4555057c60fa7412
chenyu5456 <545641154@qq.com>
2019-05-26 15:16:52 +0800

    更新

---
+++ cloud-hystrix/src/main/java/com/boot/cloud/demo/cloudhystrix/CloudHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.boot.cloud.demo.cloudhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudHystrixApplication.class, args);
+    }
+
+}
